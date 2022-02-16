136ba4379c6f296b7b3728bfe99c8f64f0f7d6e2
YuraProgStan <yurstan@gmail.com>
2022-02-16 12:25:42 +0200

    cw and hw

--- hw1/app.js
+++ hw1/app.js
@@ -8,62 +8,62 @@
 const fs = require('fs');
 const path = require('path');
 
-// fs.mkdir(path.join(__dirname, 'main'), (err) => {
-//     if(err){
-//         console.log(err);
-//         throw err;
-//     }
-// })
-// fs.mkdir(path.join(__dirname, 'main', 'online'), (err) =>{
-//     if(err){
-//         console.log(err);
-//         throw err;
-//     }
-// })
-//
-// fs.mkdir(path.join(__dirname, 'main', 'inPerson'), (err) =>{
-//     if(err){
-//         console.log(err);
-//         throw err;
-//     }
-// })
-// const onlineUsers = [{name: 'Andrii', age: 22, city: 'Lviv'},
-//     {name: 'Anna', age: 25, city: 'Kyiv'},
-//     {name: 'Sergii', age: 30, city: 'Kharkiv'}];
-// const inPersonUsers = [{name: 'Oleg', age: 32, city: 'Odessa'},
-//     {name: 'Oksana', age: 25, city: 'Kyiv'},
-//     {name: 'Maksim', age: 31, city: 'Poltava'}]
-//
-//
-// fs.writeFile(path.join(__dirname, 'main', 'online','onlineUsers.txt'), '', (err) => {
-//     if(err){
-//         console.log(err);
-//         throw err;
-//     }
-// })
-//
-// fs.writeFile(path.join(__dirname, 'main', 'inPerson','inPersonUsers.txt'), '', (err) => {
-//     if(err){
-//         console.log(err);
-//         throw err;
-//     }
-// })
-//
-// onlineUsers.map(value => fs.appendFile (path.join(__dirname, 'main', 'online','onlineUsers.txt'),
-//     `NAME:${value.name}, AGE:${value.age}, CITY: ${value.city}\n`, (err) => {
-//     if(err){
-//         console.log(err);
-//         throw err;
-//     }
-// }) )
-//
-// inPersonUsers.map(value => fs.appendFile (path.join(__dirname, 'main', 'inPerson','inPersonUsers.txt'),
-//     `NAME:${value.name}, AGE:${value.age}, CITY: ${value.city}\n`, (err) => {
-//         if(err){
-//             console.log(err);
-//             throw err;
-//         }
-//     }) )
+fs.mkdir(path.join(__dirname, 'main'), (err) => {
+    if(err){
+        console.log(err);
+        throw err;
+    }
+})
+fs.mkdir(path.join(__dirname, 'main', 'online'), (err) =>{
+    if(err){
+        console.log(err);
+        throw err;
+    }
+})
+
+fs.mkdir(path.join(__dirname, 'main', 'inPerson'), (err) =>{
+    if(err){
+        console.log(err);
+        throw err;
+    }
+})
+const onlineUsers = [{name: 'Andrii', age: 22, city: 'Lviv'},
+    {name: 'Anna', age: 25, city: 'Kyiv'},
+    {name: 'Sergii', age: 30, city: 'Kharkiv'}];
+const inPersonUsers = [{name: 'Oleg', age: 32, city: 'Odessa'},
+    {name: 'Oksana', age: 25, city: 'Kyiv'},
+    {name: 'Maksim', age: 31, city: 'Poltava'}]
+
+
+fs.writeFile(path.join(__dirname, 'main', 'online','onlineUsers.txt'), '', (err) => {
+    if(err){
+        console.log(err);
+        throw err;
+    }
+})
+
+fs.writeFile(path.join(__dirname, 'main', 'inPerson','inPersonUsers.txt'), '', (err) => {
+    if(err){
+        console.log(err);
+        throw err;
+    }
+})
+
+onlineUsers.map(value => fs.appendFile (path.join(__dirname, 'main', 'online','onlineUsers.txt'),
+    `NAME:${value.name}, AGE:${value.age}, CITY: ${value.city}\n`, (err) => {
+    if(err){
+        console.log(err);
+        throw err;
+    }
+}) )
+
+inPersonUsers.map(value => fs.appendFile (path.join(__dirname, 'main', 'inPerson','inPersonUsers.txt'),
+    `NAME:${value.name}, AGE:${value.age}, CITY: ${value.city}\n`, (err) => {
+        if(err){
+            console.log(err);
+            throw err;
+        }
+    }) )
 
 
 
