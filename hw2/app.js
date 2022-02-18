9c5ecf224cfaa90f5ba164a9190154c79e59fdda
YuraProgStan <yurstan@gmail.com>
2022-02-18 13:29:04 +0200

    hw2 and cw2 add id

--- hw2/app.js
+++ hw2/app.js
@@ -14,48 +14,54 @@ const express = require('express');
 const path = require('path');
 const {engine} = require('express-handlebars');
 
-const users = [
-    {
-        firstName: 'Sergii',
-        lastName: 'Sidorchuk',
-        email: 'test1@gmail.com',
-        password: 'passw1',
-        age: '20',
-        city: 'Kyiv'
-    },
-    {
-        firstName: 'Anna',
-        lastName: 'Stetsenko',
-        email: 'test2@gmail.com',
-        password: 'passw2',
-        age: '25',
-        city: 'Lviv'
-    },
-    {
-        firstName: 'Maksim',
-        lastName: 'Petrenko',
-        email: 'test3@gmail.com',
-        password: 'passw3',
-        age: '32',
-        city: 'Rivne'
-    },
-    {
-        firstName: 'Oksana',
-        lastName: 'Fedotova',
-        email: 'test4@gmail.com',
-        password: 'passw4',
-        age: '27',
-        city: 'Lviv'
-    },
-    {
-        firstName: 'Ostap',
-        lastName: 'Fedotova',
-        email: 'test5@gmail.com',
-        password: 'passw5',
-        age: '25',
-        city: 'Lviv'
-    }
-];
+// const users = [
+//     {
+//         firstName: 'Sergii',
+//         lastName: 'Sidorchuk',
+//         email: 'test1@gmail.com',
+//         password: 'passw1',
+//         age: '20',
+//         city: 'Kyiv',
+//         id: 1
+//     },
+//     {
+//         firstName: 'Anna',
+//         lastName: 'Stetsenko',
+//         email: 'test2@gmail.com',
+//         password: 'passw2',
+//         age: '25',
+//         city: 'Lviv',
+//         id: 2
+//     },
+//     {
+//         firstName: 'Maksim',
+//         lastName: 'Petrenko',
+//         email: 'test3@gmail.com',
+//         password: 'passw3',
+//         age: '32',
+//         city: 'Rivne',
+//         id: 3
+//     },
+//     {
+//         firstName: 'Oksana',
+//         lastName: 'Fedotova',
+//         email: 'test4@gmail.com',
+//         password: 'passw4',
+//         age: '27',
+//         city: 'Lviv',
+//         id: 4
+//     },
+//     {
+//         firstName: 'Ostap',
+//         lastName: 'Fedotova',
+//         email: 'test5@gmail.com',
+//         password: 'passw5',
+//         age: '25',
+//         city: 'Lviv',
+//         id: 5
+//     }
+// ];
+const users = [];
 let wrongEmail = '';
 const signUser = [];
 const app = express();
@@ -65,39 +71,47 @@ app.use(express.static(path.join(__dirname, 'static')));
 const methodOverride = require('method-override');
 app.use(methodOverride('_method'));
 app.set('view engine', '.hbs');
-app.engine('.hbs', engine({defaultLayout: false}));
+app.engine('.hbs', engine({defaultLayout: ''}));
 app.set('views', path.join(__dirname, 'static'));
 app.get('/login', (req, res) => {
     res.render('login');
 })
 app.get('/users', (req, res) => {
-    let newUsers;
+    if (!Object.keys(req.query).length) {
+        res.render('users', {users});
+        return;
+    }
+    let newUsers = [...users];
     if (req.query.age && req.query.city) {
-        newUsers = users.filter(value => value.age === req.query.age && value.city === req.query.city)
+        newUsers = newUsers.filter(value => value.age === req.query.age && value.city === req.query.city);
     } else if (req.query.age) {
-        newUsers = users.filter(value => value.age === req.query.age)
+        newUsers = newUsers.filter(value => value.age === req.query.age)
     } else if (req.query.city) {
-        newUsers = users.filter(value => value.city === req.query.city)
+        newUsers = newUsers.filter(value => value.city === req.query.city)
     } else {
         res.render('users', {users});
     }
     res.render('users', {users: newUsers});
-
 });
 
 app.get('/user', (req, res) => {
     res.render('user', {user: signUser});
 });
-
-app.get('/user/:userId', (req, res) => {
-    const {userId} = (req.params);
-    const user = [users[userId - 1]];
-    res.render('user', {user});
-});
-
 app.get('/error', (req, res) => {
     res.render('error', {email: wrongEmail});
 });
+
+app.get('/users/:userId', ({params}, res) => {
+    // const user = [users[userId - 1]];
+    const user = users.find(value => value.id === +params.userId);
+    if (!user) {
+        wrongEmail = `User with id: ${params.userId} does not exist`;
+        res.redirect('/error');
+        return;
+    }
+    res.render('user', {user: [user]});
+});
+
 
 app.get('/signIn', (req, res) => {
     res.render('signIn');
@@ -108,12 +122,13 @@ app.get('/wrongSignIn', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
+    const {body} = req;
     if (users.find(value => value.email === req.body.email)) {
-        wrongEmail = req.body.email;
-        res.redirect('error');
+        wrongEmail = `Write another email. This ${req.body.email} address is already in use`;
+        res.redirect('/error');
     } else {
-        users.push(req.body);
-        res.redirect('users');
+        users.push({...body, id: users.length ? users[users.length - 1].id + 1 : 1});
+        res.redirect('/users');
     }
 });
 
@@ -122,14 +137,14 @@ app.post('/signIn', (req, res) => {
         const find = users.find(value => value.email === req.body.email);
         signUser.push(find);
 
-        res.redirect('user');
+        res.redirect('/user');
     } else {
-        res.redirect('wrongSignIn');
+        res.redirect('/wrongSignIn');
     }
 
 });
 
-app.use(methodOverride( (req, res) => {
+app.use(methodOverride((req, res) => {
     const indexUsers = users.findIndex(value => value.email === req.body.email);
     users.splice(indexUsers, 1);
     const indexSignUser = signUser.findIndex(value => value.email === req.body.email);
